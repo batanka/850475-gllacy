668e01e2523ea510f64c34e6b921d5821307fb33
batanka <k.gulena@gmail.com>
2018-12-22 17:39:22 +0300

    Исправления после предзащиты

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ var close = popup.querySelector(".feedback-close");
 
 var divModal = document.createElement("div");
 
+var form = popup.querySelector("form");
+var feedbackName = popup.querySelector("[name=feedback-name]");
+var feedbackEmail = popup.querySelector("[name=feedback-email]");
+var feedbackComment = popup.querySelector("[name=feedback-comment]");
+
 // Открытие/закрытие модального окна по клику
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -15,9 +20,19 @@ link.addEventListener("click", function (evt) {
 
 });
 
+form.addEventListener("submit", function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value || !feedbackComment.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-feedback-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-feedback-error");
+  }
+});
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-feedback-show");
+  popup.classList.remove("modal-feedback-error");
   document.body.removeChild(divModal);
 
 });
@@ -27,6 +42,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-feedback-show")) {
       popup.classList.remove("modal-feedback-show");
+      popup.classList.remove("modal-feedback-error");
       document.body.removeChild(divModal);
     }
   }
